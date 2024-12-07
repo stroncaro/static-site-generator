2c9c2486f8fe743352821ee5a6a71dea708f1f32
Stéfano Troncaro <36011367+stroncaro@users.noreply.github.com>
2024-12-07 11:02:05 -0300

    refactor convert.py for simplicity

--- src/convert.py
+++ src/convert.py
@@ -3,39 +3,18 @@ from textnode import TextNode, TextType
 
 
 def text_node_to_html_node(text_node: TextNode) -> LeafNode:
-    type_func_map = {
-        TextType.NORMAL: normal,
-        TextType.BOLD: bold,
-        TextType.ITALIC: italic,
-        TextType.CODE: code,
-        TextType.LINK: link,
-        TextType.IMAGE: image,
-    }
-    func = type_func_map.get(text_node.text_type, None)
-    if func is None:
-        raise Exception("Unknown text node type")
-    return func(text=text_node.text, url=text_node.url)
-
-
-def normal(text: str, **_) -> LeafNode:
-    return LeafNode(None, text)
-
-
-def bold(text: str, **_) -> LeafNode:
-    return LeafNode("b", text)
-
-
-def italic(text: str, **_) -> LeafNode:
-    return LeafNode("i", text)
-
-
-def code(text: str, **_) -> LeafNode:
-    return LeafNode("code", text)
-
-
-def link(text: str, url: str) -> LeafNode:
-    return LeafNode("a", text, {"href": url})
-
-
-def image(text: str, url: str) -> LeafNode:
-    return LeafNode("img", "", {"src": url, "alt": text})
+    match text_node.text_type:
+        case TextType.NORMAL:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:
+            raise Exception("Unknown text node type")
